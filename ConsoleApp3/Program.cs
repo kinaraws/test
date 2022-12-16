884ab25b9ab4cb2cb600593cfd83204024f172bc
Kinara - Formulatrix <kinara.varga@formulatrix.com>
2022-12-16 14:43:54 +0700

    test

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SoalMasAdien
+{   
+    static void CheckNumber()
+    static void Main()
+    {
+        Console.WriteLine("Please input number");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.WriteLine("Number Saved");
+        }
+        else
+        {
+            Console.WriteLine("Wrong numbera");
+        }
+    }
+}
