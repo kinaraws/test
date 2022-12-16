7f70210916da10fef366b7d2d69db841edd376bf
Kinara - Formulatrix <kinara.varga@formulatrix.com>
2022-12-16 15:58:25 +0700

    Try to make it in different method, please help correct me if this is not effective yet

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,20 +2,38 @@
 
 class SoalMasAdien
 {
+    private static int n;
     static void Main()
+    { 
+        CheckNumber();
+        PrintNumber();
+        Console.WriteLine("Thank you!");
+    }
+
+    private static void CheckNumber()
     {
         Console.WriteLine("Input n number");
-        int n = Int32.Parse(Console.ReadLine());
-        int.TryParse(Console.ReadLine(), out n);
-        for (int i = 1; i < n+1; i++) 
+        string userInput = Console.ReadLine();
+        if (!int.TryParse(userInput, out n))
         {
-            if (i%3==0 && i%5==0)
+            Console.WriteLine("Please insert correct number");
+            CheckNumber();
+        }
+    }
+
+    private static void PrintNumber()
+    {
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
             {
                 Console.Write("foobar, ");
-            } else if (i%3==0)
+            }
+            else if (i % 3 == 0)
             {
                 Console.Write("foo, ");
-            } else if (i % 5 ==0)
+            }
+            else if (i % 5 == 0)
             {
                 Console.Write("bar, ");
             }
@@ -23,6 +41,8 @@ class SoalMasAdien
             {
                 Console.Write($"{i}, ");
             }
+
         }
+        return;
     }
 }
